f167448a2d0803596f22f1c2ec41b663c9e6ed03
Velmurugan Kuberan <vkuberan@outlook.com>
2016-02-18 17:17:07 +0530

    Create cs-paypal-for-woocommerce.php

---
+++ cs-paypal-for-woocommerce.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Plugin Name: Paypal for WooCommerce - Code Srushti
+ * Plugin URI: 
+ * Description: Use Paypal for collecting credit card payments on WooCommerce.
+ * Version: 1.0
+ * Author: Velmurugan Kuberan & Thiyagarajan Prabanandam
+ * Author URI: https://github.com/Code-Srushti/
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Thanks to both Stephen Zuniga & Sean Voss
+ * Stephen Zuniga // http://stephenzuniga.com
+ * Sean Voss // https://github.com/seanvoss/striper
+ *
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit; 
